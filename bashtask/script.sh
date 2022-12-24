66082b63c9d2428ca98e9f56dbc7e304721b2d5a
Egor <emoguchiy@list.ru>
2022-12-24 19:16:08 +0300

    BASH

--- bashtask/script.sh
+++ bashtask/script.sh
@@ -3,14 +3,22 @@ DIRECTORY=$*
 COUNT_ARGUMENTS=$#
 if [ $COUNT_ARGUMENTS -gt 0 ]
 then
-        for n in $DIRECTORY
+        for d in $DIRECTORY
         do
-                if [ -d $n ]
+                if [ -d $d ]
                 then
-                        cd $n
-                        ls
+                        cd $d
+                        FILE_ARR=$(ls | awk '/.txt/')
+                        echo "$FILE_ARR"
+                        for f in $FILE_ARR
+                        do
+                                ALL_NUM+=($(grep -oE "[0-9]{1,}" $f | awk '{print $1}'))
+                                SUM_FILE_NUM=$(grep -oE "[0-9]{1,}" $f | awk '{s += $1} END {print s}')
+                                SUM_ALL_NUM=$((SUM_ALL_NUM + SUM_FILE_NUM))
+                        done
+                        cd ..
                 else
-                        echo "The $n directory does not exist"
+                        echo "The $d directory does not exist"
                         exit 0
                 fi
         done
@@ -18,4 +26,15 @@ else
         echo "The number of arguments equals zero"
         exit 0
 fi
-echo "OK"
+echo "Sum of all numbers from the files: $SUM_ALL_NUM"
+
+ALL_NUM_SIZE=${#ALL_NUM[@]}
+MAX_NUM=${ALL_NUM[0]}
+for((i=0; i < $ALL_NUM_SIZE; i++))
+do
+        if [ ${ALL_NUM[i]} -gt $MAX_NUM ]
+        then
+                MAX_NUM=${ALL_NUM[i]}
+        fi
+done
+echo "Maximum number in files: $MAX_NUM"
